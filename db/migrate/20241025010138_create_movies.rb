928736578f636d52402959ec29d8f6d5d2c6c4f0
rflores <ricardo.flores@penalolen.cl>
2024-10-28 21:46:57 -0300

    último commit antes de la entrega

--- db/migrate/20241025010138_create_movies.rb
+++ db/migrate/20241025010138_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[7.2]
   def change
     create_table :movies do |t|
       t.string :name
-      t.references :client, null: false, foreign_key: true
+      t.references :client, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_25_010138) do
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
-    t.bigint "client_id", null: false
+    t.bigint "client_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_movies_on_client_id"
